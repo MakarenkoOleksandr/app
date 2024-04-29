c671dbd5ae12029eda29a6aeaf025c6c1903a05f
MakarenkoOleksandr <majop11111@gmail.com>
2024-04-29 14:39:56 +0200

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 const SERVER =
   !process.env.NODE_ENV || process.env.NODE_ENV === "development"
     ? "http://localhost:4040"
-    : "https://lemon-gorilla-slip.cyclic.app";
-
+    : "https://lemon-gorilla-slipCyclicDB";
 
 export default SERVER;
